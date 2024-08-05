a9bf42ee84f7b06913b9cc49afa3062552f67fd5
Erikamiso1 <150965664+Erikamiso1@users.noreply.github.com>
2024-08-05 08:24:39 -0300

    Update MainMenu.cpp

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,4 +1,4 @@
-#include "MainMenu.h"
+/*#include "MainMenu.h"
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
@@ -74,3 +74,93 @@ void MainMenu::MoveDown() {
 int MainMenu::MainMenuPressed() const {
     return MainMenuSelected;
 }
+*/
+
+
+
+
+
+
+
+#include "MainMenu.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+
+
+MainMenu::MainMenu(float width, float height) {
+    // Load the font
+    if (!font.loadFromFile("PressStart2P-Regular.ttf")) {
+       cerr << "No Font has been found. Please check your files properly" << endl;
+    }
+
+    // Load the background texture
+   if (!backgroundTexture.loadFromFile("Background.jpg")) {
+       cerr << "Error loading background image" << endl;
+    }
+
+    // Set the texture for the background sprite
+    backgroundSprite.setTexture(backgroundTexture);
+    backgroundSprite.setPosition(0, 0);
+
+    // Setup the main menu items
+    mainMenu[0].setFont(font);
+    mainMenu[0].setFillColor(Color::White);
+    mainMenu[0].setString("Play");
+    mainMenu[0].setCharacterSize(25);
+    mainMenu[0].setPosition(420, 130);
+
+    mainMenu[1].setFont(font);
+    mainMenu[1].setFillColor(Color::White);
+    mainMenu[1].setString("Options");
+    mainMenu[1].setCharacterSize(25);
+    mainMenu[1].setPosition(380, 230);
+
+    mainMenu[2].setFont(font);
+    mainMenu[2].setFillColor(Color::White);
+    mainMenu[2].setString("About Us");
+    mainMenu[2].setCharacterSize(25);
+    mainMenu[2].setPosition(375, 330);
+
+    mainMenu[3].setFont(font);
+    mainMenu[3].setFillColor(Color::White);
+    mainMenu[3].setString("Exit the Game");
+    mainMenu[3].setCharacterSize(25);
+    mainMenu[3].setPosition(310, 430);
+
+    MainMenuSelected = 0;
+    mainMenu[MainMenuSelected].setFillColor(Color::Black); // Highlight the first menu item
+}
+
+void MainMenu::draw(RenderWindow& window) {
+    //window.clear();
+    window.draw(backgroundSprite);
+    for (int i = 0; i < Max_main_menu; i++) {
+        window.draw(mainMenu[i]); // Draw menu items
+    }
+    //display(); // Display the contents of the window??
+}
+
+void MainMenu::MoveUp() {
+    if (MainMenuSelected - 1 >= 0) {
+        mainMenu[MainMenuSelected].setFillColor(Color::White);
+        MainMenuSelected--;
+        mainMenu[MainMenuSelected].setFillColor(Color::Black);
+    }
+}
+
+void MainMenu::MoveDown() {
+    if (MainMenuSelected + 1 < Max_main_menu) {
+        mainMenu[MainMenuSelected].setFillColor(Color::White);
+        MainMenuSelected++;
+        mainMenu[MainMenuSelected].setFillColor(Color::Black);
+    }
+}
+
+int MainMenu::MainMenuPressed() const {
+    return MainMenuSelected;
+}
+
