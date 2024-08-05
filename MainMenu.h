2f55098315d3f9633bc1433ecf0ad7894f969cf7
Erikamiso1 <150965664+Erikamiso1@users.noreply.github.com>
2024-08-05 08:24:02 -0300

    Update MainMenu.h

--- MainMenu.h
+++ MainMenu.h
@@ -1,6 +1,31 @@
 
 
 
+/*#include <SFML/Graphics.hpp>
+
+class MainMenu {
+
+private:
+    static const int Max_main_menu = 4;
+    sf::Font font;
+    sf::Text mainMenu[Max_main_menu];
+    int MainMenuSelected;
+
+    sf::Texture backgroundTexture;
+    sf::Sprite backgroundSprite;
+
+public:
+    MainMenu(float width, float height);
+    void draw(sf::RenderWindow& window);
+    void MoveUp();
+    void MoveDown();
+    int MainMenuPressed() const; // Function declaration
+
+
+};
+
+#endif*/
+
 #ifndef MAINMENU_H
 #define MAINMENU_H
 
@@ -11,6 +36,7 @@ class MainMenu {
 private:
     static const int Max_main_menu = 4;
     sf::Font font;
+    sf::Text text;
     sf::Text mainMenu[Max_main_menu];
     int MainMenuSelected;
 
